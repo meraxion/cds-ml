2d8a540b86e02de60d06d093e313c0d98b3828ac
Stian S. Grønlund <stian.groenlund@ru.nl>
2024-09-26 13:50:24 +0200

    some structure

---
+++ gradient_methods.py
@@ -0,0 +1,38 @@
+# --- Problem definition
+"""
+consider the the logistic regression problem
+the model is given in the exercise pdf, and Bishop section 4.3
+"""
+
+# Input data
+
+# Targets
+
+# probability
+
+# gradient
+
+# Hessian
+
+# --- Gradient Descent
+
+# update rule
+
+
+# early stopping
+
+
+# --- Momentum
+
+# --- Weight Decay
+
+# --- Newton Method
+
+# --- Line search
+
+# --- Conjugate gradient descent
+
+# --- Stochastic gradient descent
+
+if __name__ == "__main__":
+  print("hello world")
