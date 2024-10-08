d145f38b74f7dbc4fed3c3c9f60fca81bbdb8ef7
Femke <femke.vanvenrooij@ru.nl>
2024-10-08 08:49:07 +0200

    figures 1 and 2

--- Week 6 Deep Learning/perceptron.py
+++ Week 6 Deep Learning/perceptron.py
@@ -22,80 +22,80 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 #                     validation_data=(test_images, test_labels))
 
 # test_loss, test_acc = model.evaluate(test_images, test_labels)
+with tf.device('/CPU:0'):
+  model_0 = models.Sequential([
+    layers.Flatten(),
+    layers.Dense(10, activation="softmax")
+    ])
 
-model_0 = models.Sequential([
-  layers.Flatten(),
-  layers.Dense(10, activation="softmax")
+  model_0.compile(optimizer="adam",
+                  loss = "sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
+
+  history_0 = model_0.fit(train_images, train_labels, epochs=150,
+                        validation_data=(test_images, test_labels))
+
+  model_0_test_loss, model_0_test_acc = model_0.evaluate(test_images, test_labels)
+
+  model_1 = models.Sequential([
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(10, activation="softmax")
   ])
 
-model_0.compile(optimizer="adam",
-                loss = "sparse_categorical_crossentropy",
-                metrics=["accuracy"])
+  model_1.compile(optimizer = "adam",
+                  loss = "sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
 
-history_0 = model_0.fit(train_images, train_labels, epochs=150,
-                      validation_data=(test_images, test_labels))
+  history_1 = model_1.fit(train_images, train_labels, epochs=150,
+                        validation_data = (test_images, test_labels))
 
-model_0_test_loss, model_0_test_acc = model_0.evaluate(test_images, test_labels)
+  model_1_test_loss, model_1_test_acc = model_1.evaluate(test_images, test_labels)
 
-model_1 = models.Sequential([
-  layers.Flatten(),
-  layers.Dense(64, activation="relu"),
-  layers.Dense(10, activation="softmax")
-])
+  model_2 = models.Sequential([
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(10, activation="softmax")
+  ])
+  model_2.compile(optimizer="adam",
+                  loss = "sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
 
-model_1.compile(optimizer = "adam",
-                loss = "sparse_categorical_crossentropy",
-                metrics=["accuracy"])
+  history_2 = model_2.fit(train_images, train_labels, epochs=150,
+                        validation_data=(test_images, test_labels))
 
-history_1 = model_1.fit(train_images, train_labels, epochs=150,
-                      validation_data = (test_images, test_labels))
+  model_2_test_loss, model_2_test_acc = model_2.evaluate(test_images, test_labels)
 
-model_1_test_loss, model_1_test_acc = model_1.evaluate(test_images, test_labels)
-
-model_2 = models.Sequential([
-  layers.Flatten(),
-  layers.Dense(64, activation="relu"),
-  layers.Dense(64, activation="relu"),
-  layers.Dense(10, activation="softmax")
-])
-model_2.compile(optimizer="adam",
-                loss = "sparse_categorical_crossentropy",
-                metrics=["accuracy"])
-
-history_2 = model_2.fit(train_images, train_labels, epochs=150,
-                      validation_data=(test_images, test_labels))
-
-model_2_test_loss, model_2_test_acc = model_2.evaluate(test_images, test_labels)
-
-plt.figure(figsize=(16,10))
-for i, hist in enumerate([history_0, history_1, history_2]):
-    plt.plot(hist.history['accuracy'], label=f'accuracy model with {i} hidden layers')
-    plt.plot(hist.history['val_accuracy'], label = f'val_accuracy model with {i} hidden layers')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend(loc='lower right')
-plt.show()
+  plt.figure(figsize=(16,10))
+  for i, hist in enumerate([history_0, history_1, history_2]):
+      plt.plot(hist.history['accuracy'], label=f'accuracy model with {i} hidden layers')
+      plt.plot(hist.history['val_accuracy'], label = f'val_accuracy model with {i} hidden layers')
+      plt.xlabel('Epoch')
+      plt.ylabel('Accuracy')
+      plt.legend(loc='lower right')
+  plt.show()
 
 
-# model_20 = models.Sequential([
-#   layers.Flatten(),
-#   layers.Dense(512, activation="relu"),
-#   layers.Dense(10, activation="softmax")
-#   ])
+  model_20 = models.Sequential([
+    layers.Flatten(),
+    layers.Dense(512, activation="relu"),
+    layers.Dense(10, activation="softmax")
+    ])
 
-# model_20.compile(optimizer="adam",
-#                 loss = "sparse_categorical_crossentropy",
-#                 metrics=["accuracy"])
+  model_20.compile(optimizer="adam",
+                  loss = "sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
 
-# history_20 = model_20.fit(train_images, train_labels, epochs=1000,
-#                       validation_data=(test_images, test_labels))
+  history_20 = model_20.fit(train_images, train_labels, epochs=1000,
+                        validation_data=(test_images, test_labels))
 
-# model_20_test_loss, model_20_test_acc = model_20.evaluate(test_images, test_labels)
+  model_20_test_loss, model_20_test_acc = model_20.evaluate(test_images, test_labels)
 
-# plt.figure(figsize=(16,10))
-# plt.plot(history_20.history['accuracy'], label='accuracy')
-# plt.plot(history_20.history['val_accuracy'], label = 'val_accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.legend(loc='lower right')
-# plt.show()
+  plt.figure(figsize=(16,10))
+  plt.plot(history_20.history['accuracy'], label='accuracy')
+  plt.plot(history_20.history['val_accuracy'], label = 'val_accuracy')
+  plt.xlabel('Epoch')
+  plt.ylabel('Accuracy')
+  plt.legend(loc='lower right')
+  plt.show()
