07edf9991abebea31d785c3d0900cc12dbc92e3e
Stian S. Grønlund <stian.groenlund@ru.nl>
2024-11-13 16:30:59 +0100

    initial commit aml_as02

--- aml_as-01.py
+++ aml_as-01.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.stats as sps
 import matplotlib.pyplot as plt
-def box_muller():
+def box_muller(mu = 0, sigma=1):
   # procedure:
   # sample from a uniform distribution x1 ~ [0,1], x2 ~ [0,1]
   x = sps.uniform()
@@ -16,12 +16,17 @@ def box_muller():
     y1 = np.sqrt(-2*np.log(x1))*np.cos(2*np.pi*x2)
     y2 = np.sqrt(-2*np.log(x1))*np.sin(2*np.pi*x2)
 
-    ys[i] = y1,y2
+    ys[i] = mu + sigma*y1,y2
 
   # plot histogram of y1
-  plt.hist(ys[:,0], 100, density=True, label="")
+  plt.hist(ys[:,0], 100, density=True, label=r"Density histogram of $y$")
   # plot 1-d normal distribution over histogram
   xs = np.linspace(sps.norm.ppf(0.01),sps.norm.ppf(0.99), 100)
 
   plt.plot(xs, sps.norm.pdf(xs), "r--", label="Gaussian Probability Density")
+  plt.xlabel("y")
+  plt.ylabel(r"p(y)")
+  plt.legend()
   plt.show()
+
+box_muller()
