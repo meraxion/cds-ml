3ae524d7fdf5133c8e8ed6a92654c1b6bf56ddbb
Kazlauskaité, M. (Monika) <monika.kazlauskaite@ru.nl>
2024-11-18 19:16:36 +0100

    experiments for elongated gaussian with metropolis hastings

--- aml_as-02/elongated_gaussian.py
+++ aml_as-02/elongated_gaussian.py
@@ -17,8 +17,16 @@ def proportional_function_exponent(x):
 num_iterations = 10000
 # where to get these from? or they're completely random?
 x_init = np.array([0, 0])
+sigmas = np.linspace(0, 1, 10)
+for sigma in sigmas:
+    X, acceptance_ratio = metropolis_hastings(num_iterations, x_init, sigma, proportional_function_exponent)
 
-X = metropolis_hastings(num_iterations, x_init, proportional_function_exponent)
+    print(f"for sigma={sigma:.2f}, the ratio showing how many x were accepted: {acceptance_ratio:.4f}")
+    plt.scatter(X[:, 0], X[:, 1])
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+    plt.title(f'Elongated Gaussian, $\sigma$={sigma:.2f}, acceptance_ratio={acceptance_ratio:.4f}')
 
-plt.scatter(X[:, 0], X[:, 1])
-plt.show()
+    plt.savefig(f'elongated_gaussian_{sigma}.png')
+    plt.show()
+
--- aml_as-02/metropolis_hastings.py
+++ aml_as-02/metropolis_hastings.py
@@ -7,19 +7,25 @@ def sample_x(x_proposed, x_initial, proportional_function):
     acceptance_prob = min(1, acceptance_ratio)
     u = np.random.uniform(0, 1, size=1)
     if u <= acceptance_prob:
-        return x_proposed
+        return x_proposed, 1
     else:
-        return x_initial
+        return x_initial, 0
 
 
-def metropolis_hastings(num_iterations, x_init, proportional_function):
+def metropolis_hastings(num_iterations, x_init, sigma, proportional_function):
     X = []
+    sum_proposed = 0
     for t in range(num_iterations):
         # we always sample from normal distribution that is centered around current x
         # todo: I'm not sure if we can simply use multivariate_normal here, seems like cheating. Idk what std to use and also if I'd use my gaussian function
-        x_proposed = np.random.multivariate_normal(x_init, np.eye(2))
+        x_proposed = np.random.multivariate_normal(x_init, np.eye(2)*sigma**2)
 
-        x_sample = sample_x(x_proposed, x_init, proportional_function)
+        x_sample, num_proposed = sample_x(x_proposed, x_init, proportional_function)
         X.append(x_sample)
         x_init = x_sample
-    return np.array(X)
+
+        sum_proposed += num_proposed
+
+    # calculating the ratio of proposed x that were accepted
+    all_acceptance_ratio = sum_proposed/num_iterations
+    return np.array(X), all_acceptance_ratio
