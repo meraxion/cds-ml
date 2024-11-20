numerical instability issues hopefully fixed

aml_as-02/bayes_inf_perceptron_learning.py
@@ -19,11 +19,16 @@ xs = pd.read_csv('x.ext', sep=' ').to_numpy()[:-1].astype(float)
 @jax.jit
 def y_fn(x, w):
     wx = x @ w.T
+    # for numerical stability
+    wx = jnp.clip(wx, -50, 50)
     return 1 / (1 + jnp.exp(-wx))
 
 @jax.jit
 def G_calc(x, w, labels):
-    G = -labels * jnp.log(y_fn(x, w)) + (1 - labels) * jnp.log(1 - y_fn(x, w))
+    y = y_fn(x,w)  
+    y = jnp.clip(y, 1e-10, 1e10)
+
+    G = -labels * jnp.log(y) + (1 - labels) * jnp.log(1 - y)
     return jnp.sum(G)
 
 @jax.jit

aml_as-02/hamiltonian_mc.py
@@ -27,6 +27,10 @@ def run_leapfrog(rho, g, x, eps, energy_fn:Callable, tau):
   def scan_step(carry, _):
     rho, g, x = carry
 
+    jax.debug.print("rho: {}", rho)
+    jax.debug.print("x: {}", x)
+    jax.debug.print("g: {}", g)
+
     rho = rho - 0.5 * eps * g
     x = x + eps*rho
     g = jax.grad(energy_fn)(x)
@@ -70,7 +74,7 @@ def hmc(x0:Array,
   e = energy_fn(x0)
 
   for i in tqdm(range(n_samples-1)):
-    key, gausskey, unifkey = jax.random.split(key, 3)
+    key, gausskey, unifkey = jr.split(key, 3)
     x_new = x.at[i].get()
     rho = jr.normal(gausskey, x.shape[1])
     g_new = g
@@ -114,8 +118,8 @@ def main():
     return 0.5 * x.T@A@x
    
   n_samples = 1000
-  eps = 0.01
-  Tau = 100
+  eps = 0.001
+  Tau = 50
 
   x0 = jnp.asarray([5., 3.])
 