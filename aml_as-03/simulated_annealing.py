ae51977e00e3df0c9c9de122618d7abf55ac4dc6
Stian S. Grønlund <stian.groenlund@ru.nl>
2024-11-27 14:07:02 +0100

    prog on sim anneal

--- aml_as-03/simulated_annealing.py
+++ aml_as-03/simulated_annealing.py
@@ -51,11 +51,10 @@ def compute_acceptance(state, new_state, data, beta):
     return jnp.exp(-beta*(energy_calc(new_state, data)-energy_calc(state, data)))
 
 @jax.jit
-def sim_annealing(key, data, vec_len, R, beta, schedule:Callable, num_iterations, args):
-    state = reset(key, vec_len)
+def sim_annealing(key, state, data, vec_len, R, beta, num_iterations):
 
-    def body_fun(i, carry):
-        state, key, beta = carry
+    def body_fun(carry, tmp):
+        state, key = carry
         key, sample_key, prob_key = jr.split(key)
 
         current_state = state
@@ -63,19 +62,12 @@ def sim_annealing(key, data, vec_len, R, beta, schedule:Callable, num_iterations
         
         a = jnp.min(1, compute_acceptance(current_state, neighbour_state, 
                                           data, beta))
-
         p = jr.uniform(prob_key)
-
         state = jnp.where(a > p, neighbour_state, current_state)
-
-        new_beta = schedule(beta, *args)
-
-        return (state, key, new_beta)
+        return (state, key), state
     
-    
-    final_state, _, _ = jax.lax.fori_loop(0, num_iterations-1, body_fun, (state, key, beta))
-    
-    return final_state
+    _, states = jax.lax.scan(body_fun, (state,key), (), length=num_iterations)
+    return states
 
 def exp_schedule(beta, f):
     return f*beta
@@ -84,9 +76,30 @@ def AK_schedule(beta, delta_beta, chain_var):
     return beta + delta_beta/jnp.sqrt(chain_var)
 
 def main():
-    
-    delta_beta = 0.001
+    save = False
+    # save = True
+
+    vec_len = 500
     chain_length = 1000
+    R = 1 # neighbourhood size
+
+    data = make_data()
+    key = jr.PRNGKey(42)
+
+
+    # first, AK-schedule
+    delta_beta = 0.001
+    var_chain = 1
+    # 1 Run MH at high temperature to estimate beta_1:
+    beta_0 = 0.00001 # start with high temp=low beta
+    key, subkey = jr.split(key)
+    states = sim_annealing(subkey, data, vec_len, R, beta_0, AK_schedule, chain_length)
+
+
+
+    
+
+    
 
 
     
